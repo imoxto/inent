c5350e6ffc00eeb3a498b5f202a993f664ae4334
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2023-04-15 14:05:54 +0600

    ably working

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,14 +1,8 @@
-import { observable } from "@trpc/server/observable";
 import { z } from "zod";
-import { Realtime } from "ably";
+import * as Ably from "ably/promises";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { env } from "~/env.mjs";
-import { Message } from "@prisma/client";
-
-const ably = new Realtime.Promise(env.ABLY_API_KEY);
-ably.connection.once("connected", () => console.log("Connected to Ably!"));
-const getRoomChannel = (roomId: string) => ably.channels.get(`room:${roomId}`);
 
 console.log("Connected to Ably!");
 
@@ -63,16 +57,27 @@ export const messageRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input, ctx }) => {
-      const channel = getRoomChannel(input.roomId);
       const message = await ctx.prisma.message.create({
         data: {
           content: input.messageContent,
           roomId: input.roomId,
           userId: ctx.session.user.id,
         },
+        include: {
+          user: {
+            select: {
+              name: true,
+              image: true,
+            },
+          },
+        },
       });
 
-      await channel.publish("add", message);
+      const client = new Ably.Rest(env.ABLY_API_KEY);
+
+      var channel = client.channels.get(`roomId:${input.roomId}`);
+
+      await channel.publish("new-message", message);
     }),
 
   sendInitialDm: protectedProcedure
@@ -110,33 +115,6 @@ export const messageRouter = createTRPCRouter({
           roomId: room.id,
           userId: ctx.session.user.id,
         },
-      });
-    }),
-
-  onAdd: protectedProcedure
-    .input(z.string())
-    .subscription(async ({ input }) => {
-      const channel = getRoomChannel(input);
-      return observable<
-        Message & {
-          user: {
-            name: string | null;
-            image: string | null;
-          };
-        }
-      >((emit) => {
-        const onAdd = (message: {
-          data: Message & {
-            user: {
-              name: string | null;
-              image: string | null;
-            };
-          };
-        }) => {
-          emit.next(message.data);
-        };
-
-        channel.subscribe("add", onAdd);
       });
     }),
 });
